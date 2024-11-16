21a33dacb61e3225e26601ad94bddb51c0523b27
airi-i1998 <airi.inoue@generosity.co.jp>
2024-11-16 16:32:01 +0900

    update frontend

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,11 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
 
-createApp(App).mount('#app')
+import App from "./App.vue";
+// import router from "./router";
+import vuetify from "./plugins/vuetify";
+
+const app = createApp(App);
+// app.use(router);
+app.use(vuetify);
+
+app.mount("#app");
