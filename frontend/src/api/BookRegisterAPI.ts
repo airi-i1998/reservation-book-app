77b0e4fa1d0c68f63608f190ab064fd1a88dcaea
airi-i1998 <airi.inoue@generosity.co.jp>
2024-11-16 16:26:59 +0900

    add: bookRegister API

---
+++ frontend/src/api/BookRegisterAPI.ts
@@ -0,0 +1,17 @@
+import { ref } from "vue"
+import axios,{ AxiosError } from "axios"
+import { Book } from "../types/Book"
+
+export const useBookRegisterAPI = () => {
+  const error = ref<AxiosError>()
+
+  const bookRegister = async (bookData: Book) => {
+    try {
+      await axios.post("/api/book-register", bookData);
+    } catch (e) {
+      error.value = e as AxiosError;
+    }
+  };
+
+  return bookRegister
+}
