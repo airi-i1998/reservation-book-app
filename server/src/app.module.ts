2569dfbca36169e4c1255d29d6826d2a2ab54529
airi-i1998 <airi.inoue@generosity.co.jp>
2024-11-16 21:16:12 +0900

    add: user.module to app.module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { BookRegisterModule } from './book-register/book-register.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { BookBorrowModule } from './book-borrow/book-borrow.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [PrismaModule, BookRegisterModule, BookBorrowModule],
+  imports: [PrismaModule, BookRegisterModule, BookBorrowModule, UserModule],
   controllers: [],
   providers: [],
 })
