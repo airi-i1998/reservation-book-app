c47a5a94deb2579b1b0f36266d784108ed1b5044
airi-i1998 <airi.inoue@generosity.co.jp>
2024-12-08 17:40:47 +0900

    add: BookListView.vue

---
+++ frontend/src/views/BookListView.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import Chevron from '@/components/Chevron.vue';
+import BookSearch from '@/components/BookSearch.vue';
+import BookTable from '@/components/table/BookTable.vue';
+
+</script>
+<template>
+  <div class="book-list">
+    <Chevron />
+    <h1 class="mt-8 ml-8">書籍一覧</h1>
+    <BookSearch />
+    <div>
+      <BookTable />
+    </div>
+  </div>
+</template>
+<style scoped lang="scss">
+.book-list {
+  width: 100%;
+  height: 100%;
+}
+</style>
