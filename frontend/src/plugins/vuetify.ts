fd5c30b2c4671dd57f5526f0d2b234493a234de1
airi-i1998 <airi.inoue@generosity.co.jp>
2024-12-08 14:58:26 +0900

    add: vuetify

---
+++ frontend/src/plugins/vuetify.ts
@@ -0,0 +1,21 @@
+// Styles
+import 'vuetify/styles'
+
+// Vuetify
+import { createVuetify } from 'vuetify'
+import { colorTheme } from '@/theme/colorTheme'
+
+// import '@mdi/font/css/materialdesignicons.css' // Ensure you are using css-loader
+
+export default createVuetify({
+  // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
+  icons: {
+    defaultSet: 'mdi'
+  },
+  theme: {
+    defaultTheme: 'colorTheme',
+    themes: {
+      colorTheme
+    }
+  }
+})
