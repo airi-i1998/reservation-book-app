1a29cca23c1a3a191bd706dcc8929c0ea727254f
airi-i1998 <airi.inoue@generosity.co.jp>
2024-12-08 17:41:44 +0900

    fix: TopView.vue

--- frontend/src/views/TopView.vue
+++ frontend/src/views/TopView.vue
@@ -1,15 +1,24 @@
 <script setup lang="ts">
+import { useRouter } from 'vue-router'
+const router = useRouter()
+
+const goToBookList = () => {
+  router.push('/book/list')
+}
+
 const buttons = [
-  { name: '書籍貸し出し', color: 'secondary' },
-  { name: '書籍返却', color: 'warning' },
-  { name: '書籍登録', color: 'green' },
+  { name: '書籍貸し出し', color: 'secondary', onclick: goToBookList },
+  { name: '書籍返却', color: 'warning', onclick: goToBookList },
+  { name: '書籍登録', color: 'green', onclick: goToBookList },
 ]
 </script>
 
 <template>
   <div class="btn-container">
-    <v-btn elevation="4" rounded="xl" v-for="(button, index) in buttons"
-      style="width: 20%; height: 20%; font-size: 30px; font-weight: 600;" :key="index" :color="button.color">{{ button.name }}</v-btn>
+    <v-btn elevation="4" rounded="xl" v-for="(button, index) in buttons" :key="index" :color="button.color"
+      @click="button.onclick" style="width: 20%; height: 20%; font-size: 30px; font-weight: 600;">
+      {{ button.name }}
+    </v-btn>
   </div>
 </template>
 <style scoped lang="scss">
@@ -20,6 +29,7 @@ const buttons = [
   justify-content: space-around;
   align-items: center;
 }
+
 v-btn {
   color: #fff !important;
   font-size: 40px !important;
