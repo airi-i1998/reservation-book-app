c315e71cfbd61b29779dc609137ab378ba0ba73e
airi-i1998 <airi.inoue@generosity.co.jp>
2024-12-08 17:39:10 +0900

    add: Chevron.vue

---
+++ frontend/src/components/Chevron.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { useRouter } from 'vue-router'
+const router = useRouter()
+
+const goBackTop = () => {
+  router.push('/')
+}
+</script>
+<template>
+  <v-icon class="mt-8 ml-8" style="scale: 2" @click="goBackTop">mdi-chevron-double-left</v-icon>
+</template>
